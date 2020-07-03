06e8003d33f407b7ba7d24b0a6a1469deed49df3
Ankush Anant Kadam <nkshkdm@gmail.com>
2020-07-04 01:25:14 +0530

    Updates

---
+++ precache-manifest.44069f3a440649617f5bdb5dcd9ae693.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9468f77009d08ead618efb6d0d13e91b",
+    "url": "/react_multipage/index.html"
+  },
+  {
+    "revision": "5060287479c0c0289ee4",
+    "url": "/react_multipage/static/css/2.d04fdd5c.chunk.css"
+  },
+  {
+    "revision": "ba0198ea03b7dc5a05d0",
+    "url": "/react_multipage/static/css/main.71dc6725.chunk.css"
+  },
+  {
+    "revision": "5060287479c0c0289ee4",
+    "url": "/react_multipage/static/js/2.27468260.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react_multipage/static/js/2.27468260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0198ea03b7dc5a05d0",
+    "url": "/react_multipage/static/js/main.91af0533.chunk.js"
+  },
+  {
+    "revision": "8982ea6503584bc19a38",
+    "url": "/react_multipage/static/js/runtime-main.c81a1edc.js"
+  },
+  {
+    "revision": "35914f0d6ac8a403ba878b99ea966329",
+    "url": "/react_multipage/static/media/about.35914f0d.png"
+  },
+  {
+    "revision": "011ea4dbfb44b254358a22f4f7ebbce9",
+    "url": "/react_multipage/static/media/banner.011ea4db.png"
+  },
+  {
+    "revision": "0b502fb7f01a7a5267169c9b206a1369",
+    "url": "/react_multipage/static/media/bg1.0b502fb7.png"
+  },
+  {
+    "revision": "1da2f029f90fae55e47511e727efb4cc",
+    "url": "/react_multipage/static/media/contact.1da2f029.png"
+  }
+]);
